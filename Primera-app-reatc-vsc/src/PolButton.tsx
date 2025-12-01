0dc6bdda804ef2d95752267b20311c112d3f946e
rcastrogo <rcastrogo@hotmail.com>
2025-12-01 16:12:45 +0100

    Borrado de apps

--- Primera-app-reatc-vsc/src/PolButton.tsx
+++
@@ -1,6 +0,0 @@
-
-export default function PolButton() {
-  return (
-    <button>Hola soy un boton</button>
-  );
-};
--- sandbox/app/routes/marketing.contact.tsx
+++
@@ -1,7 +0,0 @@
-export default function MarketingContact() {
-  return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Contact page</h1>
-    </div>
-  );
-}
